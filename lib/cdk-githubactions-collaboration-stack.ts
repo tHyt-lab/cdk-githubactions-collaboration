a92ef00748cd15550acfeba771c04b5e72eb149a
tHyt-lab <hayato.tai@consallink.co.jp>
2022-09-14 08:08:50 +0000

    add: S3 Bucket

--- lib/cdk-githubactions-collaboration-stack.ts
+++ lib/cdk-githubactions-collaboration-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkGithubactionsCollaborationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkGithubactionsCollaborationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Bucket = new cdk.aws_s3.Bucket(this, 's3Bucket', {
+      removalPolicy: RemovalPolicy.DESTROY
+    })
   }
 }
+
+const app = new cdk.App()
+new CdkGithubactionsCollaborationStack(app, 'CdkGithubactionsCollaborationStack')
+app.synth()
